17a261960c8f9cf25ed94cf63b6a789801ad4c02
Sahil Marwaha <sahilm273@gmail.com>
2020-07-30 22:04:08 +0530

    Added colours

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const canvas = $("#canvas")[0];
 const ctx = canvas.getContext("2d");
 var drawing = false;
+//i've used the american spelling for color to keep it consistent lol
+var color='black';
 var x, y;
 $(document).mousedown(function(){
   drawing=true;
@@ -17,10 +19,23 @@ $("#canvas").mousemove(function(){
   ctx.beginPath();
   ctx.lineWidth = 5;
   ctx.lineCap = 'round';
-  ctx.strokeStyle = '#000000';
+  ctx.strokeStyle = color;
   ctx.moveTo(x, y);
   x = event.clientX - canvas.offsetLeft;
   y = event.clientY - canvas.offsetTop;
   ctx.lineTo(x, y);
   ctx.stroke()
+});
+
+$("button").mousedown(function(e){
+  e.preventDefault()
+});
+
+$("button").click(function(){
+  // First remove highlighting for any other colours
+  $("button").removeClass("button-selected");
+  // Then add it to whichevr one you want
+  $(this).addClass("button-selected");
+  // Now make it the active colour
+  color = this.classList[0];
 });
